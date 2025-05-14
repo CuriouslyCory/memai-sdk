0cda3c71bd8c4e1762d23b75df0cab6e58469435
Cory Sougstad <cory@hau.me>
2025-05-14 16:59:58 -0700

    Update jest config for esm modules

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
+  globals: {
+    'ts-jest': {
       useESM: true,
-    }],
+    },
   },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
